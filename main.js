0e83ae21dc48e505ba7a8b6972f94b99575b3652
SamuelVins <samuel.vins87@gmail.com>
2026-06-04 20:55:26 +0200

    operate function added

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let res;
+let num1;
+let operator;
+let num2; 
 
 function add(a, b) {
     return res = a + b;    
@@ -15,6 +18,26 @@ function multiply(a, b) {
 function divide(a, b) {
     return res = a / b;
 }
+
+function operate(a, operator, b) {
+    num1 = a;
+    operator = operator;
+    num2 = b;
+
+    operator === "+" ? add(num1, num2) :
+    operator === "-" ? subtract(num1, num2) :
+    operator === "*" ? multiply(num1, num2) :
+    operator === "/" ? divide(num1, num2) :
+    null;
+
+}
+
+// num1 = 12;
+// num2 = 5;
+// operator = "+";
+
+// operate(num1, operator, num2);
+// console.log(res);
 
 // console.log(add(6, 9));
 // multiply(5,4);
