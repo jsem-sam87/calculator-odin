6c40a45dd544946d0976366248ae5912d9174cdb
SamuelVins <samuel.vins87@gmail.com>
2026-06-04 20:31:03 +0200

    initial commit and functions +-*/

---
+++ main.js
@@ -0,0 +1,23 @@
+let res;
+
+function add(a, b) {
+    return res = a + b;    
+}
+
+function subtract(a, b) {
+    return res = a - b;    
+}
+
+function multiply(a, b) {
+    return res = a * b;
+}
+
+function divide(a, b) {
+    return res = a / b;
+}
+
+// console.log(add(6, 9));
+// multiply(5,4);
+// console.log(res);
+// add(5,4);
+// console.log(res);
